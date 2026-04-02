a2a0590c3f38625ccb97312d6dbf83688eb498f6
Alina Tabachnikas <alina.tabachnikas@skyeng.ru>
2026-04-02 19:46:01 +0300

    data for person

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
-
+    val name = "Татьяна"
+    var surname = "Андреева"
+    val middleName = "Сергеевна"
+    var age = 20
+    println("$surname $name $middleName, $age")
+    surname = "Сидорова"
+    age = 22
+    println("$surname $name $middleName, $age")
 }
