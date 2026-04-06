8331c94b79092d0f480eae78448d49873dad12dd
Alina Tabachnikas <alina.tabachnikas@skyeng.ru>
2026-04-06 20:02:36 +0300

    are conditions good for plants' growth?

--- src/Main.kt
+++ src/Main.kt
@@ -2,5 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-
+    val weatherIsSunny = true
+    val isTentOpened = true
+    val humidity = 20
+    val season = "Winter"
+    println("Благоприятные ли условия сейчас для роста бобовых? ${weatherIsSunny && isTentOpened && humidity == 20 && season != "Winter"}")
 }
